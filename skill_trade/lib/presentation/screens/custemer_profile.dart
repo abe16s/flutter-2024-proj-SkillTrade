8d92106e34e7e907167fce5a0177bae99cfbb895
0940185778 <biniyamassefa648@gmail.com>
2024-04-18 16:37:01 +0300

    edited customernprofile

--- skill_trade/lib/presentation/screens/custemer_profile.dart
+++ skill_trade/lib/presentation/screens/custemer_profile.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: camel_case_types
+// ignore_for_file: camel_case_types, prefer_final_fields, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+// ignore: unnecessary_import
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const customerProfile());
@@ -20,30 +22,76 @@ class customerProfile extends StatelessWidget {
             centerTitle: true,
             title: const Text("Customers Profile"),
           ),
-          body: const Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage("assets/profile.jpg"),
+          body: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                child: const Center(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 50.0,
+                        backgroundImage: AssetImage("assets/profile.jpg"),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Biniyam Assefa",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        "biniyamassefa648@gmail.com",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Text(
+                        "Tel: +2519-4018-5778",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 10),
-                Text(
-                  "Biniyam Assefa",
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(
-                  "biniyamassefa648@gmail.com",
-                  style: TextStyle(fontSize: 15),
-                ),
-                Text(
-                  "Tel: +2519-4018-5778",
-                  style: TextStyle(fontSize: 15),
-                ),
-              ],
-            ),
+              ),
+              const Row(
+                children: [
+                  Text(
+                    "Booked",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+              Row(
+                children: [Expanded(child: Booking())],
+              )
+            ],
           )),
+    );
+  }
+}
+
+class Booking extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Biniyam Assefa",
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            "biniyamassefa648@gmail.com",
+            style: TextStyle(fontSize: 15),
+          ),
+          Text(
+            "speciality: dish_seri",
+            style: TextStyle(fontSize: 15),
+          ),
+          Text(
+            "Tel: +2519-4018-5778",
+            style: TextStyle(fontSize: 15),
+          ),
+        ],
+      ),
     );
   }
 }
